2ccc4163978526b33b682ef77e21877ef5f1537a
Mikhail Istomin <mikhail_istomin@epam.com>
2018-01-12 13:28:08 +0300

    introduce scss

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -4,25 +4,19 @@
       <label class="search-bar__header">
         <input type="radio" name="search-by" v-model='searchBy' value="name"> by name
       </label>
-      <div class="">
-        <input :disabled='searchBy !== "name"' v-model="searchName">
-      </div>
+      <input class='search-bar__body' :disabled='searchBy !== "name"' v-model="searchName">
     </div>
     <div class="search-bar__section">
       <label class="search-bar__header">
         <input type="radio" name="search-by" v-model='searchBy' value="ingedient"> by ingredient
       </label>
-      <div class="">
-        <select :disabled='searchBy !== "ingedient"' v-model='searchIngredient'>
-          <option v-for="ing in ingredients" v-bind:value="ing">{{ing}}</option>
-        </select>
-      </div>
+      <select class='search-bar__body' :disabled='searchBy !== "ingedient"' v-model='searchIngredient'>
+        <option v-for="ing in ingredients" v-bind:value="ing">{{ing}}</option>
+      </select>
     </div>
     <div class="search-bar__section">
       <div class="search-bar__header"></div>
-      <div class="">
-        <button @click="onSearchBtnClick">Search</button>
-      </div>
+      <button class="search-bar__body search-bar__body--button" @click="onSearchBtnClick">Search</button>
     </div>
   </div>
 </template>
@@ -57,12 +51,24 @@ export default {
 
 </script>
 
-<style>
+<style lang="scss">
   .search-bar{
     display: flex;
   }
   .search-bar__header{
     height: 20px;
+    display: block;
   }
   .search-bar__switcher{}
+  .search-bar__body{
+    border: 1px solid #ccc;
+    display: block;
+    height: 20px;
+    &:disabled{
+      background: #eee;
+    }
+    &.search-bar__body--button{
+      cursor: pointer
+    }
+  }
 </style>

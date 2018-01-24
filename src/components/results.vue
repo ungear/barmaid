434bbc063ec582a509eeeb368b568314fb325321
Mikhail Istomin <mikhail_istomin@epam.com>
2018-01-24 14:06:52 +0300

    Results: styles

--- src/components/results.vue
+++ src/components/results.vue
@@ -18,10 +18,13 @@ export default {
 
 <style lang="scss" scoped>
   .found-drinks{
+    padding: 1em 0;
     .found-drinks__item{
       display: flex;
     }
     .found-drinks__item + .found-drinks__item {
+      padding-top: 1em;
+      border-top: 1px solid #ccc;
       margin-top: 1em;
     }
   }

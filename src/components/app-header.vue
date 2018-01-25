ae18a0eb06a2b62d4648a488ad14d9794ce73fd3
Mikhail Istomin <mikhail_istomin@epam.com>
2018-01-25 19:07:31 +0300

    Header: show number of favorite drinks

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -2,14 +2,26 @@
   <header class='header-row'>
     <div class='container header__header-container'>
       <router-link class='header-row__item' :to="{name:'search'}">Search</router-link>
-      <router-link class='header-row__item' :to="{name:'favorite'}">Favorite</router-link>
+      <router-link class='header-row__item marked-item' :to="{name:'favorite'}">
+        <span>Favorite</span>
+        <div class='marked-item__number-container' v-if='favoriteDrinksnumber > 0'>
+          <span class='marked-item__number'>
+            {{favoriteDrinksnumber}}
+          </span>
+        </div>
+      </router-link>
     </div>
   </header>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'app-header',
+  computed: mapState({
+    favoriteDrinksnumber: state => state.favoriteDrinkIds.length
+  }),
 }
 </script>
 
@@ -27,17 +39,30 @@ export default {
       padding-left: 40px;
     }
     .header-row__item{
-
-    }
-    a{
       color:#fff;
       text-decoration: none;
       font-weight: 900; 
     }
-    a + a{
+    .header-row__item + .header-row__item{
       margin-left: 0.5em;
     }
-
+    .marked-item{
+      display: flex;
+      align-items: center;
+      &__number-container{
+        display: flex;
+        width: 15px;
+        height: 15px;
+        align-items: center;
+        justify-content: center;
+        margin: 0 0 10px 0px;
+        border-radius: 50%;
+        background: #f5af47;
+        font-size: 0.7em;
+        padding: 3px;
+        color: #7e375f;
+      }
+    }
   }
 </style>
 

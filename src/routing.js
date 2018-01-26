8ec521464069a58a31d25eaf99bc1c6909cf4ef4
Mikhail Istomin <mikhail_istomin@epam.com>
2018-01-26 18:04:46 +0300

    fix wildcard route

--- src/routing.js
+++ src/routing.js
@@ -9,7 +9,7 @@ const routes = [
   { path: '/', name: 'search', component: Search },
   { path: '/favorite-drinks', name: 'favorite', component: lazyFavoriteDrinks },
   { path: '/drink/:link', name: 'drink',component: Drink },
-  { path: '*', redirect: { name: 'home'} },
+  { path: '*', redirect: { name: 'search'} },
 ]
 export default new VueRouter({
   routes

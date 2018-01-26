11220a397b9fed1f6cd709d98eda020c7ad760b0
Mikhail Istomin <mikhail_istomin@epam.com>
2018-01-26 15:01:45 +0300

    SearchBar: use SEARCH_BY constant instead of hardcoded strings

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -2,18 +2,18 @@
   <div class="search-bar">
     <div class="search-bar__section">
       <label class="search-bar__header search-bar__header--label">
-        <input type="radio" name="search-by" v-model='searchBy' value="name"> by name
+        <input type="radio" name="search-by" v-model='searchBy' :value="searchByTypes.name"> by name
       </label>
       <input 
         class='search-bar__body search-bar__body--first' 
-        :disabled='searchBy !== "name"' 
+        :disabled='searchBy !== searchByTypes.name' 
         v-model="searchName">
     </div>
     <div class="search-bar__section">
       <label class="search-bar__header search-bar__header--label">
-        <input type="radio" name="search-by" v-model='searchBy' value="ingedient"> by ingredient
+        <input type="radio" name="search-by" v-model='searchBy' :value="searchByTypes.ingredient"> by ingredient
       </label>
-      <select class='search-bar__body' :disabled='searchBy !== "ingedient"' v-model='searchIngredient'>
+      <select class='search-bar__body' :disabled='searchBy !== searchByTypes.ingredient' v-model='searchIngredient'>
         <option v-for="ing in ingredients" v-bind:value="ing" :key='ing'>{{ing}}</option>
       </select>
     </div>
@@ -35,7 +35,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'search-bar',
   data: function(){
-    return{
+    return {
       searchName: null,
       searchIngredient: null,
       searchBy: null,
@@ -45,14 +45,14 @@ export default {
   computed:mapState({
     searchingParamsState: state => state.searching,
     isSearchButtonActive(){
-      return this.searchBy === 'name'
+      return this.searchBy === SEARCH_BY.name
         ? !!this.searchName
         : !!this.searchIngredient
     }
   }),
   watch:{
     searchBy: function(newValue, oldValue){
-      if(newValue === 'name') this.searchIngredient = null
+      if(newValue === SEARCH_BY.name) this.searchIngredient = null
       else this.searchName = null
     }
   },
@@ -64,7 +64,7 @@ export default {
         searchBy: this.searchBy
       })
 
-      if(this.searchBy === 'name')
+      if(this.searchBy === SEARCH_BY.name)
         this.$emit('searchByName', this.searchName);
       else
         this.$emit('searchByIngredient', this.searchIngredient);
@@ -74,6 +74,7 @@ export default {
     this.searchName = this.searchingParamsState.name;
     this.searchIngredient = this.searchingParamsState.ingredient;
     this.searchBy = this.searchingParamsState.searchBy;
+    this.searchByTypes = SEARCH_BY;
     getIngredients()
       .then(ingredients => this.ingredients = ingredients)
       .catch(function (error) {})

50d3caa61ac01c99fb8ae9452a432aee9bca7bff
Mikhail Istomin <mikhail_istomin@epam.com>
2018-02-15 18:50:55 +0300

    represent some statistics

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <h3>Statistics</h3>
+    <div v-for='(ing, key) in ingredientsStatistics' :key='key'>
+      {{ing.ingredientName}} - {{ing.drinks.length}}
+    </div>
   </div>
 </template>
 <script>
-// const rawIngredientsStatistics = require('../assets/ingredients-statistics.json');
-// const ingredientsStatistics = JSON.parse(rawIngredientsStatistics);
+const ingredientsStatistics = require('../assets/ingredients-statistics.json');
+// capitalize ingredient names
+ingredientsStatistics.forEach(x => x.ingredientName = x.ingredientName[0].toUpperCase() + x.ingredientName.slice(1));
 
+const ingredientsSorted = ingredientsStatistics.sort((a,b) => {
+  //sort by drinks number then by name
+  return b.drinks.length == a.drinks.length
+    ? a.ingredientName.charCodeAt(0) - b.ingredientName.charCodeAt(0)
+    : b.drinks.length - a.drinks.length;
+})
 export default {
   name: 'about',
   data: function(){
     return{
-      //ingredientsStatistics
+      ingredientsStatistics
     }
   }  
 }

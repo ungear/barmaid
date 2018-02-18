b6083f0eae78fa0a9309528570b8bf4be8c904fe
ungear <redeemer@inbox.ru>
2018-02-18 19:09:39 +0300

    statustics draft1

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h3 class='title'>Statistics</h3>
+    <svg width='800' height='500'>
+      <circle v-for='(ing, key) in ingredientsStatistics' 
+        :key='key' 
+        :cx="Math.cos(2 * Math.PI * key/ ingredientsStatistics.length)*200 + 400" 
+        :cy="Math.sin(2 * Math.PI * key/ ingredientsStatistics.length)*200 + 250"
+        :r="3"
+        :title='ing.ingredientName'>
+        <title>{{ing.ingredientName}} - {{ing.drinks.length}}</title>
+      </circle>
+    </svg>
     <div v-for='(ing, key) in ingredientsStatistics' :key='key'>
       {{ing.ingredientName}} - {{ing.drinks.length}}
     </div>
@@ -11,12 +21,13 @@ const ingredientsStatistics = require('../assets/ingredients-statistics.json');
 // capitalize ingredient names
 ingredientsStatistics.forEach(x => x.ingredientName = x.ingredientName[0].toUpperCase() + x.ingredientName.slice(1));
 
-const ingredientsSorted = ingredientsStatistics.sort((a,b) => {
-  //sort by drinks number then by name
-  return b.drinks.length == a.drinks.length
-    ? a.ingredientName.charCodeAt(0) - b.ingredientName.charCodeAt(0)
-    : b.drinks.length - a.drinks.length;
-})
+// sort ingrediants by drinks number then name
+// ingredientsStatistics.sort((a,b) => {
+//   //sort by drinks number then by name
+//   return b.drinks.length == a.drinks.length
+//     ? a.ingredientName.charCodeAt(0) - b.ingredientName.charCodeAt(0)
+//     : b.drinks.length - a.drinks.length;
+// })
 export default {
   name: 'about',
   data: function(){

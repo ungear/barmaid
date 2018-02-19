24cca84932a4b1a3e08520cb8e04246478c15c9f
Mikhail Istomin <mikhail_istomin@epam.com>
2018-02-19 17:28:56 +0300

    drinks map

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -2,6 +2,16 @@
   <div>
     <h3 class='title'>Statistics</h3>
     <svg width='800' height='500'>
+      <circle v-for='(n, key) in drinkNodes' 
+        :key='key' 
+        :cx="n.x" 
+        :cy="n.y"
+        :r="2"
+        fill='red'
+        stroke='black'>
+        <title>{{n.strDrink}}</title>
+      </circle>
+
       <circle v-for='(ing, key, index) in ingredientsStatistics.ingredients' 
         :key='key' 
         :cx="Math.cos(2 * Math.PI * index/ ingredientsCount)*200 + 400" 
@@ -15,10 +25,20 @@
 </template>
 <script>
 const ingredientsStatistics = require('../assets/ingredients-statistics.json');
-
-
 let ingredientsCount = 0;
 for(let f in ingredientsStatistics.ingredients){ingredientsCount++}
+
+const drinkNodes = {};
+const maxX = 800;
+const maxY = 500;
+for(let dId in ingredientsStatistics.drinks){
+  let drink = ingredientsStatistics.drinks[dId]
+  drinkNodes[dId] = {
+    strDrink: drink.strDrink,
+    x: Math.ceil(Math.random() * maxX),
+    y: Math.ceil(Math.random() * maxY),
+  }
+}
 
 // sort ingrediants by drinks number then name
 // ingredientsStatistics.sort((a,b) => {
@@ -32,7 +52,8 @@ export default {
   data: function(){
     return{
       ingredientsStatistics,
-      ingredientsCount
+      ingredientsCount,
+      drinkNodes
     }
   }  
 }

5f2b01dccb87db16418800d967601fdb9817f41f
Mikhail Istomin <mikhail_istomin@epam.com>
2023-12-29 22:08:25 +0400

    add connection with firestore for simple cases

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,10 +1,19 @@
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, query, where, doc, getDoc } from 'firebase/firestore/lite';
+import { firebaseConfig } from "../../firebase-creds.mjs";
 
 const API_BASE_URL = "https://barmaid-api.ungear.ru/";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const drinksCollection = collection(db, 'drinks');
 
-export const searchDrinksByName = function(name) {
-  let url = API_BASE_URL + "drinks/by-name?like=" + name;
-  return axios.get(url).then(response => response.data);
+export const searchDrinksByName = async function(name) {
+  const drinkSnap = await getDocs(drinksCollection);
+  const result = drinkSnap.docs
+    .map(x => Object.assign(x.data(), {_id: x.id}))
+    .filter(x => x.name.toLowerCase().includes(name));
+  return result
 };
 
 export const searchDrinksByIng = function(ing) {
@@ -12,23 +21,35 @@ export const searchDrinksByIng = function(ing) {
   return axios.get(url).then(response => response.data);
 };
 
-export const getDrinkById = function(id) {
-  let url = API_BASE_URL + "drinks/" + id;
-  return axios.get(url).then(response => response.data);
+export const getDrinkById = async function(id) {
+  const docRef = doc(db, "drinks", id);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data();
 };
 
-export const getDrinkFullDataById = function(id) {
-  let url = API_BASE_URL + "drinks/full/" + id;
-  return axios.get(url).then(response => response.data);
+export const getDrinkFullDataById = async function(id) {
+  const docRef = doc(db, "drinks", id);
+  const docSnap = await getDoc(docRef);
+  const drinkData = docSnap.data();
+  drinkData._id = docSnap.id;
+  return drinkData;
 };
 
-export const getIngredients = function() {
-  let url = API_BASE_URL + "ingredients";
-  return axios.get(url).then(response => {
-    return response.data;
-    // .map(i => capitalizeFirstLetter(i.ingredientName))
-    // .sort();
+export const getIngredients = async function() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const ingCol = collection(db, 'ingredients');
+  const ingSnap = await getDocs(ingCol);
+  
+  // TODO: get rid of reactive getters and setters added in the data by the firebase engine
+  const ingData = ingSnap.docs.map(x => {
+    const ingData = x.data();
+    return {
+      _id: x.id,
+      ingredientName: ingData.name,
+    }
   });
+  return ingData;
 };
 
 function capitalizeFirstLetter(string) {

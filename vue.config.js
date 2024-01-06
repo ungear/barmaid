59ed3f3e96c8b36541dcc6ad0e380f9fde2bf1ce
Mikhail Istomin <mikhail_istomin@epam.com>
2024-01-07 00:28:03 +0400

    remove migration compatibility

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,5 @@
 module.exports = {
   chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-    
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'

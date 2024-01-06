c66daecc87643cf54ce304ff56d161d634215aab
Mikhail Istomin <mikhail_istomin@epam.com>
2024-01-06 23:48:59 +0400

    fix VUE_PROD_HYDRATION_MISMATCH_DETAILS warning

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,12 @@ module.exports = {
           }
         }
       })
+    
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions
+    })
   }
 }
